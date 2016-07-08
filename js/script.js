39468e20dc88dbfe2e1107c1648298d6a9341be0
Paha <p.balytskiy@gmail.com>
2016-07-08 16:31:40 +1000

    Save, Creat, Render are done

--- js/script.js
+++ js/script.js
@@ -1,111 +1,177 @@
-var articles = [];
+var SEQUENCE_KEY = 'article_sequence';
+var ARTICLES_KEY = 'articles';
+var articlesInRow = 2;
 
-function saveArticle() {
+
+function normalizeLocalStorageValue(key, type, defaultValue) {
+	var item = JSON.parse(localStorage.getItem(key));
+	if (typeof item !== type) {
+		localStorage.setItem(key, JSON.stringify(defaultValue));
+	}
+}
+normalizeLocalStorageValue(SEQUENCE_KEY, 'number', 1);
+normalizeLocalStorageValue(ARTICLES_KEY, 'object', {});
+
+var articles = JSON.parse(localStorage.getItem(ARTICLES_KEY));
+
+renderArticles();
+function createSaveAndShowArticle() {
 	var title = document.getElementById('articleTitle').value;
 	var content = document.getElementById('articleContent').value;
 	var article = {
-		id: 0,
 		title: title,
 		content: content,
 		creationDate: '2016-04-10',
 		author: 'Pavel Balytskiy'
 	};
-
-
-
-
-	articles.push(article);
+	saveArticle(article);
 	renderArticles();
+	clearModalFields();
 }
 
 function createHtmlArticleElement(article) {
-	var creationDateLabel = 'Создано:';
-	var authorLabel = 'Автор';
+	var creationDateLabel = 'Создано: ';
+	var authorLabel = 'Автор: ';
 	var viewDetailsBtnText = 'View details >>';
-	var articleHtml = document.createElement('article');
+
+	var articleHtml = createElement('article');
 	articleHtml.className = 'article';
-	articleHeaderHtml();
-	mainHtml();
-	footerHtml();
-	function articleHeaderHtml() {
-		var articleHeaderHtml = document.createElement('header')
-		var spanHeaderHtml = document.createElement('span');
-		spanHeaderHtml.innerHTML = creationDateLabel;
-		articleHeaderHtml.appendChild(spanHeaderHtml);
-		var timeArticleHtml = document.createElement('time');
-		timeArticleHtml.innerHTML = article.creationDate;
-		articleHeaderHtml.appendChild(timeArticleHtml);
-		var aRemoveButtonHtml = document.createElement('a');
-		var spanRemoveButtonHtml = document.createElement('span');
-		spanRemoveButtonHtml.className = 'glyphicon glyphicon-remove';
-		spanRemoveButtonHtml.setAttribute('aria-hidden', 'true');
-		aRemoveButtonHtml.appendChild(spanRemoveButtonHtml);
-		articleHeaderHtml.appendChild(aRemoveButtonHtml);
-		var aPencilButtonHtml = document.createElement('a');
-		var spanPencilButtonHtml = document.createElement('span');
-		spanPencilButtonHtml.className = 'glyphicon glyphicon-pencil';
-		spanPencilButtonHtml.setAttribute('aria-hidden', 'true');
-		aPencilButtonHtml.appendChild(spanPencilButtonHtml);
-		articleHeaderHtml.appendChild(aPencilButtonHtml);
-		articleHtml.appendChild(articleHeaderHtml);
+	articleHtml.appendChild(getHeader());
+	articleHtml.appendChild(getMain());
+	articleHtml.appendChild(getFooter());
+
+	function getHeader() {
+		var removeSpan = createElement('span');
+		removeSpan.setAttribute('aria-hidden', 'true');
+		removeSpan.className = 'glyphicon glyphicon-remove';
+
+		var pencilSpan = createElement('span');
+		pencilSpan.setAttribute('aria-hidden', 'true');
+		pencilSpan.className = 'glyphicon glyphicon-pencil';
+
+		var articleControls = createElement('div');
+		articleControls.className = 'article-controls';
+		articleControls.appendChild(createElement('a').appendChild(removeSpan));
+		articleControls.appendChild(createElement('a').appendChild(pencilSpan));
+
+		var creationLabel = createElement('span');
+		creationLabel.innerHTML = creationDateLabel;
+
+		var time = createElement('time');
+		time.innerHTML = article.creationDate;
+
+		var header = createElement('header');
+		header.appendChild(creationLabel);
+		header.appendChild(time);
+		header.appendChild(articleControls);
+		return header;
 	}
-	function mainHtml() {
-		var mainHtml = document.createElement('main');
-		var titleHtml = document.createElement('h2');
-		titleHtml.innerHTML = article.title;
-		mainHtml.appendChild(titleHtml);
-		var contentHtml = document.createElement('p');
-		contentHtml.innerHTML = article.content;
-		mainHtml.appendChild(contentHtml);
-		articleHtml.appendChild(mainHtml);
+
+	function getMain() {
+		var h2 = createElement('h2');
+		h2.innerHTML = article.title;
+
+		var p = createElement('p');
+		p.innerHTML = article.content;
+
+		var main = createElement('main');
+		main.appendChild(h2);
+		main.appendChild(p);
+		return main;
 	}
-	function footerHtml() {
-		var footerHtml = document.createElement('footer');
-		var tableHtml = document.createElement('table');
-		tableHtml.className = 'article-footer';
-		var trUpHtml = document.createElement('tr');
-		var tdAuthorHtml = document.createElement('td');
+
+	function getFooter() {
+		var authorSpan = createElement('span');
+		authorSpan.className = 'glyphicon glyphicon-user';
+		authorSpan.setAttribute('aria-hidden', 'true');
+
+		var tdAuthorHtml = createElement('td');
 		tdAuthorHtml.innerHTML = authorLabel;
-		var spanAuthorHtml = document.createElement('span');
-		spanAuthorHtml.className = 'glyphicon glyphicon-user';
-		spanAuthorHtml.setAttribute('aria-hidden', 'true');
-		tdAuthorHtml.appendChild(spanAuthorHtml);
-		trUpHtml.appendChild(tdAuthorHtml);
-		var tdButtonHtml = document.createElement('td');
-		tdButtonHtml.setAttribute('rowspan', '2');
-		var buttonHtml = document.createElement('button');
+		tdAuthorHtml.appendChild(authorSpan);
+
+		var buttonHtml = createElement('button');
 		buttonHtml.className = 'btn btn-default';
 		buttonHtml.setAttribute('type', 'button');
 		buttonHtml.innerHTML = viewDetailsBtnText;
+
+		var tdButtonHtml = createElement('td');
+		tdButtonHtml.setAttribute('rowspan', '2');
 		tdButtonHtml.appendChild(buttonHtml);
-		trUpHtml.appendChild(tdButtonHtml);
-		tableHtml.appendChild(trUpHtml);
-		var trSignure = document.createElement('tr');
-		var	tdSignure = document.createElement('td');
-		tdSignure.innerHTML = article.author;
-		trSignure.appendChild(tdSignure);
-		tableHtml.appendChild(trSignure);
-		footerHtml.appendChild(tableHtml);
-		articleHtml.appendChild(footerHtml);
+
+		var trUp = createElement('tr');
+		trUp.appendChild(tdAuthorHtml);
+		trUp.appendChild(tdButtonHtml);
+
+		var	tdSignature = createElement('td');
+		tdSignature.innerHTML = article.author;
+
+		var trSignature = createElement('tr');
+		trSignature.appendChild(tdSignature);
+
+		var table = createElement('table');
+		table.className = 'article-footer';
+		table.appendChild(trUp);
+		table.appendChild(trSignature);
+
+		var footer = createElement('footer');
+		footer.appendChild(table);
+		return footer;
 	}
-	console.log(articleHtml);
+
 	return articleHtml;
+}
+
+function saveArticle(article) {
+	function getNextId() {
+		var id = localStorage.getItem(SEQUENCE_KEY);
+		localStorage.setItem(SEQUENCE_KEY, ++id);
+		return id;
+	}
+	article.id = getNextId();
+	articles[article.id] = article;
+	localStorage.setItem(ARTICLES_KEY, JSON.stringify(articles));
+}
+
+function createElement(tagName) {
+	return document.createElement(tagName);
+}
+
+function getRow() {
+	var articleContainer = document.getElementById('articleContainer');
+	var row = articleContainer.lastElementChild;
+	var newRowNeeded = false;
+	if (row) {
+		newRowNeeded = row.childElementCount >= articlesInRow; //Возвращает количство дочерних элементов
+	} else {
+		newRowNeeded = true;
+	}
+	if (newRowNeeded) {
+		row = document.createElement('div');
+		row.className = 'row';
+		articleContainer.appendChild(row);
+	}
+	return row;
+}
+
+function renderArticle(article) {
+	var articleHtml = createHtmlArticleElement(article);
+	var colHtml = document.createElement('div');
+	colHtml.className = 'col-md-' + (12 / articlesInRow);
+	colHtml.appendChild(articleHtml);
+	var row = getRow();
+	row.appendChild(colHtml);
 }
 
 function renderArticles() {
 	var articleContainer = document.getElementById('articleContainer');
 	articleContainer.innerHTML = '';
-	var articlesInRow = 2;
-	for (i = 0; i < articles.length; i += articlesInRow) {
-		var rowHtml = document.createElement('div');
-		rowHtml.className = 'row';
-		for (j = i; j < i + articlesInRow; j++) {
-			var colHtml = document.createElement('div');
-			colHtml.className = 'col-md-6 article';
-			var articleHtml = createHtmlArticleElement(articles[j]);
-			colHtml.appendChild(articleHtml);
-			rowHtml.appendChild(colHtml);
-		}
-		articleContainer.appendChild(rowHtml);
+	for (var id in articles) {
+		if (!articles.hasOwnProperty(id)) continue;
+		renderArticle(articles[id]);
 	}
+}
+
+function clearModalFields() {
+	document.getElementById('articleTitle').value = '';
+	document.getElementById('articleContent').value = '';
 }
